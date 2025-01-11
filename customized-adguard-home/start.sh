d38619befa3565c2534462cc1b721b24939e0c58
Nathalie St <144527132+NathalieSt@users.noreply.github.com>
2025-01-11 15:26:08 +0100

    remove old adguard home

--- customized-adguard-home/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-sleep 5
-tailscale up --accept-dns=false --authkey=$TAILSCALE_AUTHKEY --hostname=adguard-home
-/opt/AdGuardHome/AdGuardHome --host 0.0.0.0
