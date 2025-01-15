9876e502c9305ffb36e3b113bc978249f9b3e992
Nathalie St <144527132+NathalieSt@users.noreply.github.com>
2025-01-15 11:03:58 +0100

    revert fix

--- cluster/apps/homepage/namespace.tf
+++ cluster/apps/homepage/namespace.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "opentofu/kubernetes"
-      version = "2.35.1"
-    }
-  }
-}
-
 resource "kubernetes_namespace" "homepage" {
   metadata {
     name = "homepage"
