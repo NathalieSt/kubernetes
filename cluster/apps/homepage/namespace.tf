7adc02128daac660aad3c8498164e1bc1bad7b71
Nathalie St <144527132+NathalieSt@users.noreply.github.com>
2025-01-15 11:00:54 +0100

    try fix error

--- cluster/apps/homepage/namespace.tf
+++ cluster/apps/homepage/namespace.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "opentofu/kubernetes"
+      version = "2.35.1"
+    }
+  }
+}
+
 resource "kubernetes_namespace" "homepage" {
   metadata {
     name = "homepage"
