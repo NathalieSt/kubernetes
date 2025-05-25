ac8550308206fcf577d2dd49c3268e0316359d14
NathalieSt <nathalie03@protonmail.com>
2025-05-25 13:12:46 +0200

    fail2ban

--- setups/vps-setup/fail2ban.sh
+++ setups/vps-setup/fail2ban.sh
@@ -6,21 +6,24 @@ sudo systemctl start fail2ban
 
 sudo systemctl enable fail2ban
 
-cat > /etc/fail2ban/jail.d/fort.conf <<EOF
-
-[DEFAULT]
-
-bantime = 5d
-
-findtime = 2d
-
-ignoreip = 127.0.0.1/8 192.168.0.0/16
-
-maxretry = 2
-
-banaction = ufw
-banaction_allports = ufw
-
-EOF
+# PERMISISION DENIED: just make it manually
+#sudo touch /etc/fail2ban/jail.d/fort.conf
+#
+#sudo cat > /etc/fail2ban/jail.d/fort.conf <<EOF
+#
+#[DEFAULT]
+#
+#bantime = 5d
+#
+#findtime = 2d
+#
+#ignoreip = 127.0.0.1/8 192.168.0.0/16
+#
+#maxretry = 2
+#
+#banaction = ufw
+#banaction_allports = ufw
+#
+#EOF
 
 sudo systemctl restart fail2ban
