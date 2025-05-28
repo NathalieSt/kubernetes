937a6249225c3a287b9b1365a078674abd00681c
NathalieSt <nathalie03@protonmail.com>
2025-05-28 15:29:05 +0200

    stuff

--- manual-deployment/netbird/install.sh
+++ manual-deployment/netbird/install.sh
@@ -2,4 +2,4 @@ kubectl create namespace netbird
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.0/cert-manager.yaml
 
-helm install netbird -f ./values.yaml -n netbird netbirdio/netbird-operator
+helm install --create-namespace -f values.yaml -n netbird netbird-operator netbirdio/kubernetes-operator
