006544b8fe604ae7faeacb81807221eeade70135
NathalieSt <nathalie03@protonmail.com>
2025-05-28 17:21:10 +0200

    try nginx

--- manual-deployment/netbird/install.sh
+++ manual-deployment/netbird/install.sh
@@ -1,5 +1,8 @@
 kubectl create namespace netbird
+kubectl apply -f ../../../api.yaml
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.0/cert-manager.yaml
+
+sleep 2m
 
 helm install --create-namespace -f values.yaml -n netbird netbird-operator netbirdio/kubernetes-operator
