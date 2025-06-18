56afac7f2a0e5c8cd1aa674c3ecb981fde93c42d
NathalieSt <nathalie03@protonmail.com>
2025-06-18 07:53:45 +0200

    start for vps setup for minecraft

---
+++ setups/minecraft-server/fail2ban.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install fail2ban
+
+sudo apt install python3-systemd
+
+sudo systemctl start fail2ban
+
+sudo systemctl enable fail2ban
+
+# PERMISISION DENIED: just make it manually
+#sudo touch /etc/fail2ban/jail.d/fort.conf
+#
+#sudo cat > /etc/fail2ban/jail.d/fort.conf <<EOF
+#
+#[DEFAULT]
+#
+#bantime = 5d
+#
+#findtime = 2d
+#
+#ignoreip = 127.0.0.1/8 192.168.0.0/16
+#
+#maxretry = 2
+#
+#banaction = ufw
+#banaction_allports = ufw
+#
+#EOF
+
+sudo systemctl restart fail2ban
---
+++ setups/minecraft-server/ufw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# PREREQUISITE: sudoer user
+# run this file with sudo
+
+sudo apt update
+sudo apt install ufw
+
+sudo ufw allow in ssh
+
+# allow netbird
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 33073/tcp
+sudo ufw allow 10000/tcp
+sudo ufw allow 33080/tcp
+sudo ufw allow 3478/udp
+sudo ufw allow 49152:65535/udp
