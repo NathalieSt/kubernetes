92c128fd0078ab5bd4964717bc26f6a22dcf7f1d
NathalieSt <nathalie03@protonmail.com>
2025-08-07 22:58:52 +0200

    use outdent

--- setups/vps-setup/nfs.sh
+++ setups/vps-setup/nfs.sh
@@ -9,3 +9,10 @@ sudo nano /etc/exports
 sudo systemctl restart nfs-kernel-server
 
 sudo systemctl status nfs-kernel-server
+
+# 100.127.1.1/16 <- is netbird address specific
+sudo ufw allow in from 100.127.1.1/16 to any port 111
+sudo ufw allow in from 100.127.1.1/16 to any port 2049
+# 13025 <- this ports needs to be manually fixed, its usually dynamic
+# change it in /etc/nfs.conf under [mountd]
+sudo ufw allow in from 100.127.1.1/16 to any port 13025
