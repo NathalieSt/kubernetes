63939c512273b6d03bd2d05454ab32b1932c2e04
NathalieSt <nathalie03@protonmail.com>
2025-09-07 18:47:10 +0200

    generate cnpg via go

--- internal/generators/infrastructure/cnpg/manifests.go
+++ internal/generators/infrastructure/cnpg/manifests.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"kubernetes/internal/generators"
-	"kubernetes/internal/generators/infrastructure"
 	"kubernetes/internal/pkg/utils"
 	"kubernetes/pkg/schema/cluster/flux/helm"
-	"kubernetes/pkg/schema/cluster/flux/oci"
 	"kubernetes/pkg/schema/generator"
 	"kubernetes/pkg/schema/k8s/meta"
 )
@@ -14,24 +11,42 @@ import (
 func createCNPGManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
 	namespace := utils.ManifestConfig{
 		Filename:  "namespace.yaml",
-		Manifests: utils.GenerateNamespace(generatorMeta.Namespace, true),
+		Manifests: utils.GenerateNamespace(generatorMeta.Namespace, false),
 	}
 
 	repoName := fmt.Sprintf("%v-repo", generatorMeta.Name)
 	repo := utils.ManifestConfig{
 		Filename: "repo.yaml",
 		Manifests: []any{
-			oci.NewRepo(
+			helm.NewRepo(
 				meta.ObjectMeta{
-					Name: repoName,
+					Name: generatorMeta.Name,
 				},
-				oci.RepoSpec{
+				helm.RepoSpec{
+					RepoType: helm.Default,
 					Url:      generatorMeta.Helm.Url,
 					Interval: "24h",
-					Ref: oci.RepoRef{
-						Tag: generatorMeta.Helm.Version,
-					},
-				}),
+				},
+			),
+		},
+	}
+
+	chartName := fmt.Sprintf("%v-chart", generatorMeta.Name)
+	chart := utils.ManifestConfig{
+		Filename: "chart.yaml",
+		Manifests: []any{
+			helm.NewChart(meta.ObjectMeta{
+				Name: chartName,
+			}, helm.ChartSpec{
+				Interval:          "24h",
+				Chart:             generatorMeta.Helm.Chart,
+				ReconcileStrategy: helm.ChartVersion,
+				SourceRef: helm.ChartSourceRef{
+					Kind: helm.HelmRepository,
+					Name: repoName,
+				},
+				Version: generatorMeta.Helm.Version,
+			}),
 		},
 	}
 
@@ -47,68 +62,16 @@ func createCNPGManifests(generatorMeta generator.GeneratorMeta) map[string][]byt
 					Interval:    "24h",
 					Timeout:     "10m",
 					ChartRef: helm.ReleaseChartRef{
-						Kind: helm.OCIRepository,
-						Name: repoName,
+						Kind: helm.HelmChart,
+						Name: chartName,
 					},
 					Install: helm.ReleaseInstall{
 						Remediation: helm.ReleaseInstallRemediation{
 							Retries: 3,
 						},
 					},
-					ValuesFrom: []helm.ReleaseValuesFrom{
-						{
-							Kind:       helm.Secret,
-							Name:       generators.PostgresCredsSecret,
-							ValuesKey:  "username",
-							TargetPath: "gitea.config.database.USER",
-							Optional:   false,
-						},
-						{
-							Kind:       helm.Secret,
-							Name:       generators.PostgresCredsSecret,
-							ValuesKey:  "password",
-							TargetPath: "gitea.config.database.PASSWD",
-							Optional:   false,
-						},
-					},
-					Values: map[string]any{
-						"gitea": map[string]any{
-							"admin": map[string]any{"username": "Nathi"},
-							"config": map[string]any{
-								"database": map[string]any{
-									"DB_TYPE": "postgres",
-									"HOST":    fmt.Sprintf("%v:%v", infrastructure.Postgres.ClusterUrl, infrastructure.Postgres.Port),
-									"NAME":    "forgejo",
-								},
-								"server": map[string]any{
-									"ROOT_URL": "https://code.cluster.netbird.selfhosted",
-								},
-							},
-							"queue": map[string]any{
-								"TYPE":     "redis",
-								"CONN_STR": "valkey://valkey.valkey.svc.cluster.local:6379/0?",
-							},
-							"cache": map[string]any{
-								"ADAPTER": "redis",
-								"HOST":    "valkey://valkey.valkey.svc.cluster.local:6379/1",
-							},
-							"session": map[string]any{
-								"PROVIDER":        "redis",
-								"PROVIDER_CONFIG": "valkey://valkey.valkey.svc.cluster.local:6379/2",
-							},
-						},
-						"persistence": map[string]any{
-							"enabled":      true,
-							"storageClass": generators.NFSRemoteClass,
-						},
-					},
 				}),
 		},
-	}
-
-	scaledObject := utils.ManifestConfig{
-		Filename:  "scaled-object.yaml",
-		Manifests: utils.GenerateCronScaler(fmt.Sprintf("%v-scaledobject", generatorMeta.Name), generatorMeta.Name, generatorMeta.KedaScaling),
 	}
 
 	kustomization := utils.ManifestConfig{
@@ -118,11 +81,11 @@ func createCNPGManifests(generatorMeta generator.GeneratorMeta) map[string][]byt
 			[]string{
 				namespace.Filename,
 				repo.Filename,
+				chart.Filename,
 				release.Filename,
-				scaledObject.Filename,
 			},
 		),
 	}
 
-	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, release, scaledObject})
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, chart, release})
 }
