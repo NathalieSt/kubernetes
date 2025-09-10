aa99ecf1ca70b03c954aebaad3b08b4c24ab96ed
NathalieSt <nathalie03@protonmail.com>
2025-09-10 19:59:30 +0200

    generate perses, use generic function to generate helm manifests for reflector

--- internal/generators/monitoring/perses/manifests.go
+++ internal/generators/monitoring/perses/manifests.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"kubernetes/internal/generators"
 	"kubernetes/internal/pkg/utils"
 	"kubernetes/pkg/schema/generator"
+	"kubernetes/pkg/schema/k8s/apps"
+	"kubernetes/pkg/schema/k8s/core"
+	"kubernetes/pkg/schema/k8s/meta"
 )
 
 func createPersesManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
@@ -11,12 +16,124 @@ func createPersesManifests(generatorMeta generator.GeneratorMeta) map[string][]b
 		Manifests: utils.GenerateNamespace(generatorMeta.Namespace, true),
 	}
 
+	pvcName := fmt.Sprintf("%v-pvc", generatorMeta.Name)
+	pvc := utils.ManifestConfig{
+		Filename: "pvc.yaml",
+		Manifests: []any{
+			core.NewPersistentVolumeClaim(meta.ObjectMeta{
+				Name: pvcName,
+			}, core.PersistentVolumeClaimSpec{
+				AccessModes: []string{"ReadWriteMany"},
+				Resources: core.VolumeResourceRequirements{Requests: map[string]string{
+					"storage": "8Gi",
+				}},
+				StorageClassName: generators.NFSRemoteClass,
+			},
+			),
+		},
+	}
+
+	volumeName := "storage-volumec"
+	deployment := utils.ManifestConfig{
+		Filename: "deployment.yaml",
+		Manifests: []any{
+			apps.NewDeployment(
+				meta.ObjectMeta{
+					Name: generatorMeta.Name,
+					Labels: map[string]string{
+						"app.kubernetes.io/name":    generatorMeta.Name,
+						"app.kubernetes.io/version": generatorMeta.Docker.Version,
+					},
+				},
+				apps.DeploymentSpec{
+					Replicas: 1,
+					Selector: meta.LabelSelector{
+						MatchLabels: map[string]string{
+							"app.kubernetes.io/name": generatorMeta.Name,
+						},
+					},
+					Template: core.PodTemplateSpec{
+						Metadata: meta.ObjectMeta{
+							Labels: map[string]string{
+								"app.kubernetes.io/name":    generatorMeta.Name,
+								"app.kubernetes.io/version": generatorMeta.Docker.Version,
+							},
+						},
+						Spec: core.PodSpec{
+							Containers: []core.Container{
+								{
+									Name:  generatorMeta.Name,
+									Image: fmt.Sprintf("%v:%v", generatorMeta.Docker.Registry, generatorMeta.Docker.Version),
+									Ports: []core.Port{
+										{
+											ContainerPort: generatorMeta.Port,
+											Name:          generatorMeta.Name,
+										},
+									},
+									VolumeMounts: []core.VolumeMount{
+										{
+											MountPath: "/etc/perses/storage",
+											Name:      volumeName,
+										},
+									},
+								},
+							},
+							Volumes: []core.Volume{
+								{
+									Name: volumeName,
+									PersistentVolumeClaim: core.PVCVolumeSource{
+										ClaimName: pvcName,
+									},
+								},
+							},
+						},
+					},
+				},
+			),
+		},
+	}
+
+	service := utils.ManifestConfig{
+		Filename: "service.yaml",
+		Manifests: []any{
+			core.NewService(
+				meta.ObjectMeta{
+					Name: generatorMeta.Name,
+					Labels: map[string]string{
+						"app.kubernetes.io/name":    generatorMeta.Name,
+						"app.kubernetes.io/version": generatorMeta.Docker.Version,
+					},
+				}, core.ServiceSpec{
+					Selector: map[string]string{
+						"app.kubernetes.io/name": generatorMeta.Name,
+					},
+					Ports: []core.ServicePort{
+						{
+							Name:       fmt.Sprintf("http-%v", generatorMeta.Name),
+							Port:       generatorMeta.Port,
+							TargetPort: generatorMeta.Port,
+						},
+					},
+				},
+			),
+		},
+	}
+
+	scaledObject := utils.ManifestConfig{
+		Filename:  "scaled-object.yaml",
+		Manifests: utils.GenerateCronScaler(fmt.Sprintf("%v-scaledobject", generatorMeta.Name), generatorMeta.Name, generatorMeta.KedaScaling),
+	}
+
 	kustomization := utils.ManifestConfig{
 		Filename: "kustomization.yaml",
 		Manifests: utils.GenerateKustomization(generatorMeta.Name, []string{
 			namespace.Filename,
+			pvc.Filename,
+			deployment.Filename,
+			service.Filename,
+			scaledObject.Filename,
 		}),
 	}
 
-	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization})
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, pvc, deployment, service, scaledObject})
 }
