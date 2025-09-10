6698fd142259c6e8b4154429c1d61147e1f5341b
NathalieSt <nathalie03@protonmail.com>
2025-09-10 10:52:57 +0200

    generate reflector via go

--- internal/generators/infrastructure/reflector/manifests.go
+++ internal/generators/infrastructure/reflector/manifests.go
@@ -3,15 +3,13 @@ package main
 import (
 	"fmt"
 	"kubernetes/internal/generators"
-	"kubernetes/internal/generators/infrastructure"
 	"kubernetes/internal/pkg/utils"
 	"kubernetes/pkg/schema/cluster/flux/helm"
-	"kubernetes/pkg/schema/cluster/flux/oci"
 	"kubernetes/pkg/schema/generator"
 	"kubernetes/pkg/schema/k8s/meta"
 )
 
-func createForgejoManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
+func createReflectorManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
 	namespace := utils.ManifestConfig{
 		Filename:  "namespace.yaml",
 		Manifests: utils.GenerateNamespace(generatorMeta.Namespace, true),
@@ -21,108 +19,104 @@ func createForgejoManifests(generatorMeta generator.GeneratorMeta) map[string][]
 	repo := utils.ManifestConfig{
 		Filename: "repo.yaml",
 		Manifests: []any{
-			oci.NewRepo(
-				meta.ObjectMeta{
-					Name: repoName,
-				},
-				oci.RepoSpec{
+			helm.NewRepo(meta.ObjectMeta{
+				Name: repoName,
+			},
+				helm.RepoSpec{
+					RepoType: helm.Default,
 					Url:      generatorMeta.Helm.Url,
 					Interval: "24h",
-					Ref: oci.RepoRef{
-						Tag: generatorMeta.Helm.Version,
-					},
 				}),
+		},
+	}
+
+	chartName := fmt.Sprintf("%v-chart", generatorMeta.Name)
+	chart := utils.ManifestConfig{
+		Filename: "chart.yaml",
+		Manifests: []any{
+			helm.NewChart(meta.ObjectMeta{
+				Name: chartName,
+			}, helm.ChartSpec{
+				Interval:          "24h",
+				Chart:             generatorMeta.Helm.Chart,
+				ReconcileStrategy: helm.ChartVersion,
+				SourceRef: helm.ChartSourceRef{
+					Kind: helm.HelmRepository,
+					Name: repoName,
+				},
+				Version: generatorMeta.Helm.Version,
+			}),
 		},
 	}
 
 	release := utils.ManifestConfig{
 		Filename: "release.yaml",
 		Manifests: []any{
-			helm.NewRelease(
-				meta.ObjectMeta{
-					Name: generatorMeta.Name,
-				},
+			helm.NewRelease(meta.ObjectMeta{
+				Name: generatorMeta.Name,
+			},
 				helm.ReleaseSpec{
 					ReleaseName: generatorMeta.Name,
 					Interval:    "24h",
 					Timeout:     "10m",
 					ChartRef: helm.ReleaseChartRef{
-						Kind: helm.OCIRepository,
-						Name: repoName,
+						Kind: helm.HelmChart,
+						Name: chartName,
 					},
 					Install: helm.ReleaseInstall{
 						Remediation: helm.ReleaseInstallRemediation{
 							Retries: 3,
 						},
 					},
-					ValuesFrom: []helm.ReleaseValuesFrom{
-						{
-							Kind:       helm.Secret,
-							Name:       generators.PostgresCredsSecret,
-							ValuesKey:  "username",
-							TargetPath: "gitea.config.database.USER",
-							Optional:   false,
-						},
-						{
-							Kind:       helm.Secret,
-							Name:       generators.PostgresCredsSecret,
-							ValuesKey:  "password",
-							TargetPath: "gitea.config.database.PASSWD",
-							Optional:   false,
-						},
-					},
-					Values: map[string]any{
-						"gitea": map[string]any{
-							"admin": map[string]any{"username": "Nathi"},
-							"config": map[string]any{
-								"database": map[string]any{
-									"DB_TYPE": "postgres",
-									"HOST":    fmt.Sprintf("%v:%v", infrastructure.Postgres.ClusterUrl, infrastructure.Postgres.Port),
-									"NAME":    "forgejo",
-								},
-								"server": map[string]any{
-									"ROOT_URL": "https://code.cluster.netbird.selfhosted",
-								},
-							},
-							"queue": map[string]any{
-								"TYPE":     "redis",
-								"CONN_STR": "valkey://valkey.valkey.svc.cluster.local:6379/0?",
-							},
-							"cache": map[string]any{
-								"ADAPTER": "redis",
-								"HOST":    "valkey://valkey.valkey.svc.cluster.local:6379/1",
-							},
-							"session": map[string]any{
-								"PROVIDER":        "redis",
-								"PROVIDER_CONFIG": "valkey://valkey.valkey.svc.cluster.local:6379/2",
-							},
-						},
-						"persistence": map[string]any{
-							"enabled":      true,
-							"storageClass": generators.NFSRemoteClass,
-						},
-					},
+					Values: map[string]any{},
 				}),
 		},
 	}
 
-	scaledObject := utils.ManifestConfig{
-		Filename:  "scaled-object.yaml",
-		Manifests: utils.GenerateCronScaler(fmt.Sprintf("%v-scaledobject", generatorMeta.Name), generatorMeta.Name, generatorMeta.KedaScaling),
+	netbirdSecretConfig := utils.StaticSecretConfig{
+		Name:       fmt.Sprintf("%v-netbird-static-secret", generatorMeta.Name),
+		SecretName: generators.NetbirdNecretName,
+		Path:       "netbird/setup-key",
+		SecretAnnotations: map[string]string{
+			"reflector.v1.k8s.emberstack.com/reflection-allowed":            "true",
+			"reflector.v1.k8s.emberstack.com/reflection-allowed-namespaces": "caddy",
+			"reflector.v1.k8s.emberstack.com/reflection-auto-enabled":       "true",
+			"reflector.v1.k8s.emberstack.com/reflection-auto-namespaces":    "caddy",
+		},
+	}
+
+	postgresSecretConfig := utils.StaticSecretConfig{
+		Name:       fmt.Sprintf("%v-postgres-static-secret", generatorMeta.Name),
+		SecretName: generators.PostgresCredsSecret,
+		Path:       "postgres",
+		SecretAnnotations: map[string]string{
+			"reflector.v1.k8s.emberstack.com/reflection-allowed":            "true",
+			"reflector.v1.k8s.emberstack.com/reflection-allowed-namespaces": "postgres,dawarich,mealie,forgejo,keycloak",
+			"reflector.v1.k8s.emberstack.com/reflection-auto-enabled":       "true",
+			"reflector.v1.k8s.emberstack.com/reflection-auto-namespaces":    "postgres,dawarich,mealie,forgejo,keycloak",
+		},
+	}
+
+	vaultSecrets := utils.ManifestConfig{
+		Filename: "vault-secrets.yaml",
+		Manifests: utils.GenerateVaultAccessManifests(
+			generatorMeta.Name,
+			//FIXME: get this from VSO generator meta
+			"vault-secrets-operator",
+			[]utils.StaticSecretConfig{netbirdSecretConfig, postgresSecretConfig},
+		),
 	}
 
 	kustomization := utils.ManifestConfig{
 		Filename: "kustomization.yaml",
-		Manifests: utils.GenerateKustomization(
-			generatorMeta.Name,
-			[]string{
-				namespace.Filename,
-				repo.Filename,
-				release.Filename,
-				scaledObject.Filename,
-			},
-		),
+		Manifests: utils.GenerateKustomization(generatorMeta.Name, []string{
+			namespace.Filename,
+			repo.Filename,
+			chart.Filename,
+			release.Filename,
+			vaultSecrets.Filename,
+		}),
 	}
 
-	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, release, scaledObject})
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, chart, release, vaultSecrets})
 }
--- internal/generators/infrastructure/reflector/meta.go
+++ internal/generators/infrastructure/reflector/meta.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"kubernetes/pkg/schema/cluster/infrastructure/keda"
 	"kubernetes/pkg/schema/generator"
 )
 
-var Forgejo = generator.GeneratorMeta{
-	Name:          "forgejo",
-	Namespace:     "forgejo",
-	GeneratorType: generator.App,
-	ClusterUrl:    "forgejo.forgejo.svc.cluster.local",
-	Port:          9000,
+var Reflector = generator.GeneratorMeta{
+	Name:          "reflector",
+	Namespace:     "reflector",
+	GeneratorType: generator.Infrastructure,
 	Helm: generator.Helm{
-		Url:     "oci://code.forgejo.org/forgejo-helm/forgejo",
-		Version: "14.0.0",
-	},
-	KedaScaling: keda.ScaledObjectTriggerMeta{
-		Timezone:        "Europe/Vienna",
-		Start:           "0 9 * * *",
-		End:             "0 23 * * *",
-		DesiredReplicas: "1",
+		Chart:   "reflector",
+		Url:     "https://emberstack.github.io/helm-charts",
+		Version: "9.1.27",
 	},
 	DependsOnGenerators: []string{},
 }
