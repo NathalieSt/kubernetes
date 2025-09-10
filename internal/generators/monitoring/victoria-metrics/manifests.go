7918cccb8b4f0743122f5c9caa7bb4daba39521b
NathalieSt <nathalie03@protonmail.com>
2025-09-10 14:56:59 +0200

    generate victoria-metrics via go

--- internal/generators/monitoring/victoria-metrics/manifests.go
+++ internal/generators/monitoring/victoria-metrics/manifests.go
@@ -5,6 +5,7 @@ import (
 	"kubernetes/internal/generators"
 	"kubernetes/internal/pkg/utils"
 	"kubernetes/pkg/schema/cluster/flux/helm"
+	"kubernetes/pkg/schema/cluster/flux/oci"
 	"kubernetes/pkg/schema/generator"
 	"kubernetes/pkg/schema/k8s/meta"
 )
@@ -19,92 +20,55 @@ func createVictoriaMetricsManifests(generatorMeta generator.GeneratorMeta) map[s
 	repo := utils.ManifestConfig{
 		Filename: "repo.yaml",
 		Manifests: []any{
-			helm.NewRepo(meta.ObjectMeta{
-				Name: repoName,
-			},
-				helm.RepoSpec{
-					RepoType: helm.Default,
-					Url:      generatorMeta.Helm.Url,
-					Interval: "24h",
-				}),
-		},
-	}
-
-	chartName := fmt.Sprintf("%v-chart", generatorMeta.Name)
-	chart := utils.ManifestConfig{
-		Filename: "chart.yaml",
-		Manifests: []any{
-			helm.NewChart(meta.ObjectMeta{
-				Name: chartName,
-			}, helm.ChartSpec{
-				Interval:          "24h",
-				Chart:             generatorMeta.Helm.Chart,
-				ReconcileStrategy: helm.ChartVersion,
-				SourceRef: helm.ChartSourceRef{
-					Kind: helm.HelmRepository,
+			oci.NewRepo(
+				meta.ObjectMeta{
 					Name: repoName,
 				},
-				Version: generatorMeta.Helm.Version,
-			}),
+				oci.RepoSpec{
+					Url:      generatorMeta.Helm.Url,
+					Interval: "24h",
+					Ref: oci.RepoRef{
+						Tag: generatorMeta.Helm.Version,
+					},
+				}),
 		},
 	}
 
 	release := utils.ManifestConfig{
 		Filename: "release.yaml",
 		Manifests: []any{
-			helm.NewRelease(meta.ObjectMeta{
-				Name: generatorMeta.Name,
-			},
+			helm.NewRelease(
+				meta.ObjectMeta{
+					Name: generatorMeta.Name,
+				},
 				helm.ReleaseSpec{
 					ReleaseName: generatorMeta.Name,
 					Interval:    "24h",
 					Timeout:     "10m",
 					ChartRef: helm.ReleaseChartRef{
-						Kind: helm.HelmChart,
-						Name: chartName,
+						Kind: helm.OCIRepository,
+						Name: repoName,
 					},
 					Install: helm.ReleaseInstall{
 						Remediation: helm.ReleaseInstallRemediation{
 							Retries: 3,
 						},
 					},
-					Values: map[string]any{},
-				}),
+					Values: map[string]any{
+						"nameOverride":      "vmks",
+						"grafana":           map[string]any{"enabled": false},
+						"defaultDashboards": map[string]any{"enabled": false},
+						"vmsingle": map[string]any{
+							"spec": map[string]any{
+								"storage": map[string]any{
+									"storageClassName": generators.NFSRemoteClass,
+								},
+							},
+						},
+					},
+				},
+			),
 		},
-	}
-
-	netbirdSecretConfig := utils.StaticSecretConfig{
-		Name:       fmt.Sprintf("%v-netbird-static-secret", generatorMeta.Name),
-		SecretName: generators.NetbirdNecretName,
-		Path:       "netbird/setup-key",
-		SecretAnnotations: map[string]string{
-			"reflector.v1.k8s.emberstack.com/reflection-allowed":            "true",
-			"reflector.v1.k8s.emberstack.com/reflection-allowed-namespaces": "caddy",
-			"reflector.v1.k8s.emberstack.com/reflection-auto-enabled":       "true",
-			"reflector.v1.k8s.emberstack.com/reflection-auto-namespaces":    "caddy",
-		},
-	}
-
-	postgresSecretConfig := utils.StaticSecretConfig{
-		Name:       fmt.Sprintf("%v-postgres-static-secret", generatorMeta.Name),
-		SecretName: generators.PostgresCredsSecret,
-		Path:       "postgres",
-		SecretAnnotations: map[string]string{
-			"reflector.v1.k8s.emberstack.com/reflection-allowed":            "true",
-			"reflector.v1.k8s.emberstack.com/reflection-allowed-namespaces": "postgres,dawarich,mealie,forgejo,keycloak",
-			"reflector.v1.k8s.emberstack.com/reflection-auto-enabled":       "true",
-			"reflector.v1.k8s.emberstack.com/reflection-auto-namespaces":    "postgres,dawarich,mealie,forgejo,keycloak",
-		},
-	}
-
-	vaultSecrets := utils.ManifestConfig{
-		Filename: "vault-secrets.yaml",
-		Manifests: utils.GenerateVaultAccessManifests(
-			generatorMeta.Name,
-			//FIXME: get this from VSO generator meta
-			"vault-secrets-operator",
-			[]utils.StaticSecretConfig{netbirdSecretConfig, postgresSecretConfig},
-		),
 	}
 
 	kustomization := utils.ManifestConfig{
@@ -112,11 +76,9 @@ func createVictoriaMetricsManifests(generatorMeta generator.GeneratorMeta) map[s
 		Manifests: utils.GenerateKustomization(generatorMeta.Name, []string{
 			namespace.Filename,
 			repo.Filename,
-			chart.Filename,
 			release.Filename,
-			vaultSecrets.Filename,
 		}),
 	}
 
-	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, chart, release, vaultSecrets})
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, release})
 }
