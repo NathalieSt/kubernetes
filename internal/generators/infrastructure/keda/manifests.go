1e4d0bb65ac4331845fddae236dc7946d1a65829
NathalieSt <nathalie03@protonmail.com>
2025-09-18 09:23:00 +0200

    fix release values for keda

--- internal/generators/infrastructure/keda/manifests.go
+++ internal/generators/infrastructure/keda/manifests.go
@@ -14,19 +14,21 @@ func createKedaManifests(generatorMeta generator.GeneratorMeta) map[string][]byt
 	repo, chart, release := utils.GetGenericHelmDeploymentManifests(generatorMeta.Name, generatorMeta.Helm,
 		map[string]any{
 			// Pod annotations for KEDA operator
-			"keda": map[string]any{
-				"traffic.sidecar.istio.io/excludeInboundPorts":  "9666",
-				"traffic.sidecar.istio.io/excludeOutboundPorts": "9443,6443",
-			},
-			// Pod annotations for KEDA Metrics Adapter
-			"metricsAdapter": map[string]any{
-				"traffic.sidecar.istio.io/excludeInboundPorts":  "6443",
-				"traffic.sidecar.istio.io/excludeOutboundPorts": "9666,9443",
-			},
-			// Pod annotations for KEDA Admission webhooks
-			"webhooks": map[string]any{
-				"traffic.sidecar.istio.io/excludeInboundPorts":  "9443",
-				"traffic.sidecar.istio.io/excludeOutboundPorts": "9666,6443",
+			"podAnnotations": map[string]any{
+				"keda": map[string]any{
+					"traffic.sidecar.istio.io/excludeInboundPorts":  "9666",
+					"traffic.sidecar.istio.io/excludeOutboundPorts": "9443,6443",
+				},
+				// Pod annotations for KEDA Metrics Adapter
+				"metricsAdapter": map[string]any{
+					"traffic.sidecar.istio.io/excludeInboundPorts":  "6443",
+					"traffic.sidecar.istio.io/excludeOutboundPorts": "9666,9443",
+				},
+				// Pod annotations for KEDA Admission webhooks
+				"webhooks": map[string]any{
+					"traffic.sidecar.istio.io/excludeInboundPorts":  "9443",
+					"traffic.sidecar.istio.io/excludeOutboundPorts": "9666,6443",
+				},
 			},
 		},
 		nil,
