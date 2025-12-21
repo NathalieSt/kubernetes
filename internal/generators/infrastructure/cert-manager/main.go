3ed1fefe087bb476b37f09a0b65ae0328c80e783
NathalieSt <nathalie03@protonmail.com>
2025-12-21 10:22:25 +0100

    add cert-manager

---
+++ internal/generators/infrastructure/cert-manager/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"kubernetes/internal/pkg/utils"
+	"kubernetes/pkg/schema/generator"
+	"path/filepath"
+)
+
+func main() {
+	flags := utils.GetGeneratorFlags()
+	if flags == nil {
+		fmt.Println("An error happened while getting flags for generator")
+		return
+	}
+
+	name := "cert-manager"
+	generatorType := generator.Infrastructure
+	meta := generator.GeneratorMeta{
+		Name:          name,
+		Namespace:     "cert-manager",
+		GeneratorType: generatorType,
+		Helm: &generator.Helm{
+			Chart:   "cert-manager",
+			Url:     "oci://quay.io/jetstack/charts/cert-manager",
+			Version: utils.GetGeneratorVersionByType(flags.RootDir, name, generatorType),
+		},
+		DependsOnGenerators: []string{},
+	}
+
+	utils.RunGenerator(utils.GeneratorRunnerConfig{
+		Meta:             meta,
+		ShouldReturnMeta: flags.ShouldReturnMeta,
+		OutputDir:        filepath.Join(flags.RootDir, "/cluster/infrastructure/cert-manager/"),
+		CreateManifests:  createCertManagerManifests,
+	})
+}
---
+++ internal/generators/infrastructure/cert-manager/manifests.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"kubernetes/internal/pkg/utils"
+	"kubernetes/pkg/schema/cluster/flux/helm"
+	"kubernetes/pkg/schema/cluster/flux/oci"
+	"kubernetes/pkg/schema/generator"
+	"kubernetes/pkg/schema/k8s/meta"
+)
+
+func createCertManagerManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
+	namespace := utils.ManifestConfig{
+		Filename:  "namespace.yaml",
+		Manifests: utils.GenerateNamespace(generatorMeta.Namespace),
+	}
+
+	repoName := fmt.Sprintf("%v-repo", generatorMeta.Name)
+	repo := utils.ManifestConfig{
+		Filename: "repo.yaml",
+		Manifests: []any{
+			oci.NewRepo(
+				meta.ObjectMeta{
+					Name: repoName,
+				},
+				oci.RepoSpec{
+					Url:      generatorMeta.Helm.Url,
+					Interval: "24h",
+					Ref: oci.RepoRef{
+						Tag: generatorMeta.Helm.Version,
+					},
+				}),
+		},
+	}
+
+	release := utils.ManifestConfig{
+		Filename: "release.yaml",
+		Manifests: []any{
+			helm.NewRelease(
+				meta.ObjectMeta{
+					Name: generatorMeta.Name,
+				},
+				helm.ReleaseSpec{
+					ReleaseName: generatorMeta.Name,
+					Interval:    "24h",
+					Timeout:     "10m",
+					ChartRef: helm.ReleaseChartRef{
+						Kind: helm.OCIRepository,
+						Name: repoName,
+					},
+					Install: helm.ReleaseInstall{
+						Remediation: helm.ReleaseInstallRemediation{
+							Retries: 3,
+						},
+					},
+					Values: map[string]any{},
+				}),
+		},
+	}
+
+	kustomization := utils.ManifestConfig{
+		Filename: "kustomization.yaml",
+		Manifests: utils.GenerateKustomization(
+			generatorMeta.Name,
+			[]string{
+				namespace.Filename,
+				repo.Filename,
+				release.Filename,
+			},
+		),
+	}
+
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, release})
+}
