be5d5cf61d7f84ad8775681ba326ad6bde75f1a6
NathalieSt <nathalie03@protonmail.com>
2025-12-24 18:00:38 +0100

    first attempt of adding elk-stack

---
+++ internal/generators/monitoring/elastic-stack/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"kubernetes/internal/pkg/utils"
+	"kubernetes/pkg/schema/generator"
+	"path/filepath"
+)
+
+func main() {
+	flags := utils.GetGeneratorFlags()
+	if flags == nil {
+		fmt.Println("An error happened while getting flags for generator")
+		return
+	}
+
+	name := "elastic-stack"
+	generatorType := generator.Monitoring
+	meta := generator.GeneratorMeta{
+		Name:          name,
+		Namespace:     "elastic-stack",
+		GeneratorType: generatorType,
+		Helm: &generator.Helm{
+			Url:     "https://helm.elastic.co",
+			Chart:   "eck-stack",
+			Version: utils.GetGeneratorVersionByType(flags.RootDir, name, generatorType),
+		},
+		DependsOnGenerators: []string{},
+	}
+
+	utils.RunGenerator(utils.GeneratorRunnerConfig{
+		Meta:             meta,
+		ShouldReturnMeta: flags.ShouldReturnMeta,
+		OutputDir:        filepath.Join(flags.RootDir, "/cluster/monitoring/elastic-stack/"),
+		CreateManifests:  createElasticStackManifests,
+	})
+}
---
+++ internal/generators/monitoring/elastic-stack/manifests.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"kubernetes/internal/generators"
+	"kubernetes/internal/pkg/utils"
+	"kubernetes/pkg/schema/generator"
+)
+
+func createElasticStackManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
+	namespace := utils.ManifestConfig{
+		Filename:  "namespace.yaml",
+		Manifests: utils.GenerateNamespace(generatorMeta.Namespace),
+	}
+
+	repo, chart, release := utils.GetGenericHelmDeploymentManifests(generatorMeta.Name, generatorMeta.Helm,
+		map[string]any{
+			"elasticsearch": map[string]any{
+				"spec": map[string]any{
+					"nodeSets": map[string]any{
+						"volumeClaimTemplates": []map[string]any{
+							{
+								"metadata": map[string]any{
+									"name": "elasticsearch-data",
+								},
+								"spec": map[string]any{
+									"accessModes": []string{
+										"ReadWriteOnce",
+									},
+									"storageClassName": generators.NFSRemoteClass,
+									"resources": map[string]any{
+										"requests": map[string]any{
+											"storage": "20Gi",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"eck-beats": map[string]any{
+				"enabled": true,
+				"type":    "filebeat",
+			},
+		},
+		nil,
+	)
+
+	kustomization := utils.ManifestConfig{
+		Filename: "kustomization.yaml",
+		Manifests: utils.GenerateKustomization(generatorMeta.Name, []string{
+			namespace.Filename,
+			repo.Filename,
+			chart.Filename,
+			release.Filename,
+		}),
+	}
+
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, repo, chart, release})
+}
