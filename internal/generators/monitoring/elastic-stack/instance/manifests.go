949199c1a567c35f38f3406fbbe56818b9daa054
NathalieSt <nathalie03@protonmail.com>
2025-12-24 20:40:48 +0100

    try to fix beats

--- internal/generators/monitoring/elastic-stack/instance/manifests.go
+++ internal/generators/monitoring/elastic-stack/instance/manifests.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"kubernetes/internal/generators"
 	"kubernetes/internal/pkg/utils"
 	"kubernetes/pkg/schema/generator"
+	"os"
 )
 
 func createElasticStackManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
 	namespace := utils.ManifestConfig{
 		Filename:  "namespace.yaml",
 		Manifests: utils.GenerateNamespace(generatorMeta.Namespace),
+	}
+
+	config, err := os.ReadFile("./beats-config.yaml")
+	if err != nil {
+		fmt.Printf("Error while reading config.yaml")
 	}
 
 	repo, chart, release := utils.GetGenericHelmDeploymentManifests(generatorMeta.Name, generatorMeta.Helm,
@@ -42,12 +49,14 @@ func createElasticStackManifests(generatorMeta generator.GeneratorMeta) map[stri
 				"enabled": true,
 				"type":    "filebeat",
 				"elasticsearchRef": map[string]any{
-					"name": "elasticsearch-es-default",
+					"name": "elasticsearch",
 				},
 				"kibanaRef": map[string]any{
-					"name": "elastic-stack-instance-eck-kibana-kb",
+					"name": "elastic-stack-instance-eck-kibana",
 				},
-
+				"config": map[string]any{
+					"filebeat.yml": string(config),
+				},
 				"daemonSet": map[string]any{
 					"podTemplate": map[string]any{
 						"spec": map[string]any{
