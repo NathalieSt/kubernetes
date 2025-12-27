bd87be8edefde61b33488e5a6a9e3ccf69f851e7
NathalieSt <nathalie03@protonmail.com>
2025-12-27 14:15:37 +0100

    add lidify

---
+++ internal/generators/apps/lidify/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"kubernetes/internal/pkg/utils"
+	"kubernetes/pkg/schema/cluster/infrastructure/keda"
+	"kubernetes/pkg/schema/generator"
+	"path/filepath"
+)
+
+func main() {
+	flags := utils.GetGeneratorFlags()
+	if flags == nil {
+		fmt.Println("An error happened while getting flags for generator")
+		return
+	}
+
+	name := "lidify"
+	generatorType := generator.App
+	meta := generator.GeneratorMeta{
+		Name:          name,
+		Namespace:     "lidify",
+		GeneratorType: generatorType,
+		ClusterUrl:    "lidify.lidify.svc.cluster.local",
+		Port:          3030,
+		Docker: &generator.Docker{
+			Registry: "chevron7locked/lidify",
+			Version:  utils.GetGeneratorVersionByType(flags.RootDir, name, generatorType),
+		},
+		Caddy: &generator.Caddy{
+			DNSName:                    "jellyfin",
+			WebsocketSupportIsRequired: true,
+		},
+		KedaScaling: &keda.ScaledObjectTriggerMeta{
+			Timezone:        "Europe/Vienna",
+			Start:           "0 7 * * *",
+			End:             "0 1 * * *",
+			DesiredReplicas: "1",
+		},
+		DependsOnGenerators: []string{},
+	}
+
+	utils.RunGenerator(utils.GeneratorRunnerConfig{
+		Meta:             meta,
+		ShouldReturnMeta: flags.ShouldReturnMeta,
+		OutputDir:        filepath.Join(flags.RootDir, "/cluster/apps/lidify/"),
+		CreateManifests:  createLidifyManifests,
+	})
+}
---
+++ internal/generators/apps/lidify/manifests.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"fmt"
+	"kubernetes/internal/generators"
+	"kubernetes/internal/pkg/utils"
+	"kubernetes/pkg/schema/cluster/infrastructure/keda"
+	"kubernetes/pkg/schema/generator"
+	"kubernetes/pkg/schema/k8s/apps"
+	"kubernetes/pkg/schema/k8s/core"
+	"kubernetes/pkg/schema/k8s/meta"
+)
+
+func createLidifyManifests(generatorMeta generator.GeneratorMeta) map[string][]byte {
+	namespace := utils.ManifestConfig{
+		Filename:  "namespace.yaml",
+		Manifests: utils.GenerateNamespace(generatorMeta.Namespace),
+	}
+
+	musicPVCName := fmt.Sprintf("%v-music-pvc", generatorMeta.Name)
+	musicPVC := utils.ManifestConfig{
+		Filename: "music-pvc.yaml",
+		Manifests: []any{
+			core.NewPersistentVolumeClaim(meta.ObjectMeta{
+				Name: musicPVCName,
+			}, core.PersistentVolumeClaimSpec{
+				AccessModes: []string{"ReadWriteMany"},
+				Resources: core.VolumeResourceRequirements{Requests: map[string]string{
+					"storage": "100Gi",
+				}},
+				StorageClassName: generators.NFSLocalClass,
+			}),
+		},
+	}
+
+	configPVCName := fmt.Sprintf("%v-config-pvc", generatorMeta.Name)
+	configPVC := utils.ManifestConfig{
+		Filename: "config-pvc.yaml",
+		Manifests: []any{
+			core.NewPersistentVolumeClaim(meta.ObjectMeta{
+				Name: configPVCName,
+			}, core.PersistentVolumeClaimSpec{
+				AccessModes: []string{"ReadWriteMany"},
+				Resources: core.VolumeResourceRequirements{Requests: map[string]string{
+					"storage": "30Gi",
+				}},
+				StorageClassName: generators.NFSRemoteClass,
+			}),
+		},
+	}
+
+	configVolumeName := "config-volume"
+	musicVolumeName := "music-volume"
+	deployment := utils.ManifestConfig{
+		Filename: "deployment.yaml",
+		Manifests: []any{
+			apps.NewDeployment(
+				meta.ObjectMeta{
+					Name: generatorMeta.Name,
+					Labels: map[string]string{
+						"app.kubernetes.io/name":    generatorMeta.Name,
+						"app.kubernetes.io/version": generatorMeta.Docker.Version,
+					},
+				},
+				apps.DeploymentSpec{
+					Replicas: 1,
+					Selector: meta.LabelSelector{
+						MatchLabels: map[string]string{
+							"app.kubernetes.io/name": generatorMeta.Name,
+						},
+					},
+					Template: core.PodTemplateSpec{
+						Metadata: meta.ObjectMeta{
+							Labels: map[string]string{
+								"app.kubernetes.io/name":    generatorMeta.Name,
+								"app.kubernetes.io/version": generatorMeta.Docker.Version,
+							},
+						},
+						Spec: core.PodSpec{
+							Containers: []core.Container{
+								{
+									Name:  generatorMeta.Name,
+									Image: fmt.Sprintf("%v:%v", generatorMeta.Docker.Registry, generatorMeta.Docker.Version),
+									Ports: []core.Port{
+										{
+											ContainerPort: generatorMeta.Port,
+											Name:          generatorMeta.Name,
+										},
+									},
+									VolumeMounts: []core.VolumeMount{
+										{
+											MountPath: "/music",
+											Name:      musicVolumeName,
+										},
+										{
+											MountPath: "/data",
+											Name:      configPVCName,
+										},
+									},
+								},
+							},
+							Volumes: []core.Volume{
+								{
+									Name: configVolumeName,
+									PersistentVolumeClaim: core.PVCVolumeSource{
+										ClaimName: configPVCName,
+									},
+								}, {
+									Name: musicPVCName,
+									PersistentVolumeClaim: core.PVCVolumeSource{
+										ClaimName: musicPVCName,
+									},
+								},
+							},
+						},
+					},
+				},
+			),
+		},
+	}
+
+	service := utils.ManifestConfig{
+		Filename: "service.yaml",
+		Manifests: []any{
+			core.NewService(
+				meta.ObjectMeta{
+					Name: generatorMeta.Name,
+					Labels: map[string]string{
+						"app.kubernetes.io/name":    generatorMeta.Name,
+						"app.kubernetes.io/version": generatorMeta.Docker.Version,
+					},
+					Annotations: map[string]string{
+						"netbird.io/expose": "true",
+						"netbird.io/groups": "cluster-services",
+					},
+				}, core.ServiceSpec{
+					Selector: map[string]string{
+						"app.kubernetes.io/name": generatorMeta.Name,
+					},
+					Ports: []core.ServicePort{
+						{
+							Name:       fmt.Sprintf("http-%v", generatorMeta.Name),
+							Port:       generatorMeta.Port,
+							TargetPort: generatorMeta.Port,
+						},
+					},
+				},
+			),
+		},
+	}
+
+	scaledObject := utils.ManifestConfig{
+		Filename:  "scaled-object.yaml",
+		Manifests: utils.GenerateCronScaler(fmt.Sprintf("%v-scaledobject", generatorMeta.Name), generatorMeta.Name, keda.Deployment, generatorMeta.KedaScaling),
+	}
+
+	kustomization := utils.ManifestConfig{
+		Filename: "kustomization.yaml",
+		Manifests: utils.GenerateKustomization(generatorMeta.Name, []string{
+			namespace.Filename,
+			deployment.Filename,
+			service.Filename,
+			configPVC.Filename,
+			musicPVC.Filename,
+			scaledObject.Filename,
+		}),
+	}
+
+	return utils.MarshalManifests([]utils.ManifestConfig{namespace, kustomization, deployment, service, configPVC, musicPVC, scaledObject})
+}
