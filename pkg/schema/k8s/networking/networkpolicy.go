df6b5b478b17774a4840712665f547d3488b913f
NathalieSt <nathalie03@protonmail.com>
2025-12-31 12:33:23 +0100

    add go code for defining network policies

---
+++ pkg/schema/k8s/networking/networkpolicy.go
@@ -0,0 +1,65 @@
+package networking
+
+import (
+	"kubernetes/pkg/schema/k8s/meta"
+	"kubernetes/pkg/schema/shared"
+)
+
+type IPBlock struct {
+	CIDR   string   `yaml:"cidr,omitempty"`
+	Except []string `yaml:"except,omitempty"`
+}
+
+type NetworkPolicyPeer struct {
+	IpBlock           IPBlock            `yaml:"ipBlock,omitempty"`
+	NamespaceSelector meta.LabelSelector `yaml:"namespaceSelector,omitempty"`
+	PodSelector       meta.LabelSelector `yaml:"podSelector,omitempty"`
+}
+
+type PortProtocol string
+
+const (
+	SCTP PortProtocol = "SCTP"
+	TCP  PortProtocol = "TCP"
+	UDP  PortProtocol = "UDP"
+)
+
+type NetworkPolicyPort struct {
+	Port     int32        `yaml:"port,omitempty"`
+	EndPort  int32        `yaml:"endPort,omitempty"`
+	Protocol PortProtocol `yaml:"protocol,omitempty"`
+}
+
+type NetworkPolicyEgressRule struct {
+	To    []NetworkPolicyPeer `yaml:"to,omitempty"`
+	Ports []NetworkPolicyPort `yaml:"ports,omitempty"`
+}
+
+type NetworkPolicyIngressRule struct {
+	From  []NetworkPolicyPeer `yaml:"from,omitempty"`
+	Ports []NetworkPolicyPort `yaml:"ports,omitempty"`
+}
+
+type NetworkPolicySpec struct {
+	PodSelector meta.LabelSelector         `yaml:"podSelector,omitempty"`
+	PolicyTypes []string                   `yaml:"policyTypes,omitempty"`
+	Ingress     []NetworkPolicyIngressRule `yaml:"ingress,omitempty"`
+	Egress      []NetworkPolicyEgressRule  `yaml:"egress,omitempty"`
+}
+
+type NetworkPolicy struct {
+	shared.CommonK8sResourceWithSpec[NetworkPolicySpec] `yaml:",omitempty,inline" validate:"required"`
+}
+
+func NewNetworkPolicy(meta meta.ObjectMeta, spec NetworkPolicySpec) NetworkPolicy {
+	return NetworkPolicy{
+		CommonK8sResourceWithSpec: shared.CommonK8sResourceWithSpec[NetworkPolicySpec]{
+			CommonK8sResource: shared.CommonK8sResource{
+				ApiVersion: "networking.k8s.io/v1",
+				Kind:       "NetworkPolicy",
+				Metadata:   meta,
+			},
+			Spec: spec,
+		},
+	}
+}
