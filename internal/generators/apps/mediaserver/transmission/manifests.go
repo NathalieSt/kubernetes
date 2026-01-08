a5d7e720ea75a4e101cf87d7b4a888df9da3cd31
NathalieSt <nathalie03@protonmail.com>
2026-01-08 22:14:54 +0100

    set guid and uid for flood

--- internal/generators/apps/mediaserver/transmission/manifests.go
+++ internal/generators/apps/mediaserver/transmission/manifests.go
@@ -127,6 +127,10 @@ func createTransmissionManifests(generatorMeta generator.GeneratorMeta) map[stri
 							},
 						},
 						Spec: core.PodSpec{
+							SecurityContext: core.PodSecurityContext{
+								RunAsUser:  1000,
+								RunAsGroup: 1001,
+							},
 							InitContainers: []core.Container{
 								{
 									Name:  "config-init",
