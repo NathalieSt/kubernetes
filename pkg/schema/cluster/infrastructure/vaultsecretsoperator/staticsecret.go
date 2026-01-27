4c00350ded88914362fc5a2b660cedbcfaed46af
NathalieSt <nathalie03@protonmail.com>
2026-01-27 14:41:30 +0100

    add Transformation to VaultStaticSecret Destination

--- pkg/schema/cluster/infrastructure/vaultsecretsoperator/staticsecret.go
+++ pkg/schema/cluster/infrastructure/vaultsecretsoperator/staticsecret.go
@@ -12,13 +12,41 @@ const (
 	BasicAuth SecretType = "kubernetes.io/basic-auth"
 )
 
-type Destination struct {
-	Create      bool              `yaml:"create,omitempty"`
-	Name        string            `yaml:"name,omitempty"`
-	Labels      map[string]string `yaml:"labels,omitempty"`
-	Annotations map[string]string `yaml:"annotations,omitempty"`
-	Type        SecretType        `yaml:"type,omitempty"`
+type Template struct {
+	Name string `yaml:"name,omitempty"`
+	Text string `yaml:"text,omitempty"`
 }
+
+type TemplateRef struct {
+	Name        string `yaml:"name,omitempty"`
+	KeyOverride string `yaml:"keyOverride,omitempty"`
+}
+
+type TransformationRef struct {
+	Namespace      string        `yaml:"namespace,omitempty"`
+	Name           string        `yaml:"name,omitempty"`
+	TemplateRefs   []TemplateRef `yaml:"templateRefs,omitempty"`
+	IgnoreIncludes bool          `yaml:"ignoreIncludes,omitempty"`
+	IgnoreExcludes bool          `yaml:"ignoreExcludes,omitempty"`
+}
+
+type Transformation struct {
+	Templates          map[string]Template `yaml:"templates,omitempty"`
+	TransformationRefs []TransformationRef `yaml:"transformationRefs,omitempty"`
+	Includes           []string            `yaml:"includes,omitempty"`
+	Excludes           []string            `yaml:"excludes,omitempty"`
+	ExcludeRaw         bool                `yaml:"excludeRaw,omitempty"`
+}
+
+type Destination struct {
+	Create         bool              `yaml:"create,omitempty"`
+	Name           string            `yaml:"name,omitempty"`
+	Labels         map[string]string `yaml:"labels,omitempty"`
+	Annotations    map[string]string `yaml:"annotations,omitempty"`
+	Type           SecretType        `yaml:"type,omitempty"`
+	Transformation Transformation    `yaml:"transformation,omitempty"`
+}
+
 type StaticSecretSpec struct {
 	AuthRef      string      `yaml:"vaultAuthRef,omitempty"`
 	Mount        string      `yaml:"mount,omitempty"`
